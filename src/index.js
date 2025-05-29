1461a0a3a8d78ce4397814f8f9fa0fc9aea2b183
mahendra-madavali <mahendra.madavali@onehubpos.com>
2025-05-29 01:01:31 -0700

    Add 2 numbers functionality added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import AddNumbers  from './App';
+import AddNumbers  from './AddNumbers';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
